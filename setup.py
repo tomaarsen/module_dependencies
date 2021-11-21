198f54225c70b6d5cc9245ea86c597907e6b60c6
Tom Aarsen <Cubiegamedev@gmail.com>
2021-11-21 14:45:51 +0100

    Create setup.py for installing module_dependencies
    Note: Not on PyPI yet

---
+++ setup.py
@@ -0,0 +1,30 @@
+from io import open
+from setuptools import setup, find_packages
+
+setup(
+    name='module_dependencies',
+    version='0.1.0',
+    url='https://github.com/tomaarsen/module_dependencies',
+    license='MIT',
+    author='Tom Aarsen',
+    author_email='ta.aarsen@gmail.com',
+    description='Gather module dependencies of source code',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    zip_safe=False,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    include_package_data=True,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
