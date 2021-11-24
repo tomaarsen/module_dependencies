d376d00f2bbf4fba3751e96aa6a3bb7c0cd38cb9
Tom Aarsen <Cubiegamedev@gmail.com>
2021-11-24 21:20:34 +0100

    Increment version to accompany the recent refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="module_dependencies",
-    version="0.1.4",
+    version="0.1.5",
     url="https://github.com/tomaarsen/module_dependencies",
     license="MIT",
     author="Tom Aarsen",
