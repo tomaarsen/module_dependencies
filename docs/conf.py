8101744a4d8e28778b651a73dbb06c9168a5286d
Tom Aarsen <Cubiegamedev@gmail.com>
2022-01-03 17:14:09 +0100

    Prepare for initial release

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Tom Aarsen"
 author = "Tom Aarsen"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.5"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
