6a3af77c1d58ee6792c43643ab108c38b4878f4f
Tom Aarsen <Cubiegamedev@gmail.com>
2022-01-03 19:32:44 +0100

    Increment version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Tom Aarsen"
 author = "Tom Aarsen"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
